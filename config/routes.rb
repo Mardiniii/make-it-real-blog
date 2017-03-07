3727399c4dc925e46ce5c5ca099735d4a35ace39
mardiniii <sezama03@hotmail.com>
2017-03-07 18:17:32 -0500

    Clean routes and add link to new post

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
   root 'posts#index'
 
-  get 'posts/new'
-
-  get 'posts/create'
-
-  get 'posts/edit'
-
-  get 'posts/update'
-
-  get 'posts/destroy'
-
-  get 'posts/show'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts
 end
