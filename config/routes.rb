aaf2b6243f5dc64b9c1c9e1b84b1c5bc9b99576f
mardiniii <sezama03@hotmail.com>
2017-03-06 20:03:00 -0500

    Generate Posts controller, views and initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
+  get 'posts/show'
+
+  get 'posts/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
