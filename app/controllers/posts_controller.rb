81a58189feb24c1fb7ee949e6633d26e59254b39
mardiniii <sezama03@hotmail.com>
2017-03-07 18:56:10 -0500

    Define create action for posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,13 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
