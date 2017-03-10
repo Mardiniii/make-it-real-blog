e22052c026db05f614ede755bae876e74b22c6ad
mardiniii <sezama03@hotmail.com>
2017-03-09 19:41:39 -0500

    Add post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,4 +11,7 @@
 #
 
 class Post < ApplicationRecord
+  validates :author, :title, :content, presence: true
+  validates :title, length: { in: 5...50 }
+  validates :content, length: { in: 50...500 }
 end
