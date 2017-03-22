786811457dfe26a82af6ad742e7e8387b8de5548
mardiniii <sezama03@hotmail.com>
2017-03-21 19:15:30 -0500

    Add methods to send mails

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,2 +1,16 @@
 class UserNotifierMailer < ApplicationMailer
+  default from: 'info@makeitrealblog.com'
+
+  def welcome_user(user)
+    @user = user
+    @url = 'https://make-it-real-blog-sebas.herokuapp.com/posts'
+    mail(to: @user.email, subject: 'Bienvenido al Blog De Make It Real')
+  end
+
+  def new_post_mail(user, post)
+    @user = user
+    @post = post
+
+    mail(to: @user.email, subject: 'Un nuevo post ha sido creado en Make It Real Blog')
+  end
 end
